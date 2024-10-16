9a69a1e8451ad297cef7d1fff5aa31fee68a0cfd
vaibhav <vaibhav.goyal@giva.co>
2024-10-16 21:46:12 +0530

    again bug fix

--- api/index.js
+++ api/index.js
@@ -817,7 +817,7 @@ app.put(
   }
 );
 
-// export const handler = serverless(app);
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+export const handler = serverless(app);
+// app.listen(3000, () => {
+//   console.log("Server running on port 3000");
+// });
